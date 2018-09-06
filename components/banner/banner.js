f6a4d92d30996251e65a23db333f52028e6e0ec9
nullcss <nullyl@163.com>
2018-09-06 10:19:14 +0800

    css

---
+++ components/banner/banner.js
@@ -0,0 +1,23 @@
+// components/banner/banner.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    bannerBg: '../../images/index/bannerBg.jpg'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
