afe846950a45526c7f83fc485eec45c79cdc2c6c
nullcss <nullyl@163.com>
2018-09-06 11:23:32 +0800

    css

---
+++ components/design/design.js
@@ -0,0 +1,26 @@
+// components/design/design.js
+let text = `圆融建筑结构特有的空间属性，在满足安全和功能的前提下，增强学生的体验和互动，启发学生主动思考、学习和探索。`
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    introduceBg: '../../images/index/designBg.jpg',
+    round: '../../images/index/round.png',
+    text: text
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
