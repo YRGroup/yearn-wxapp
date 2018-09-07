9cde5a2e0d9ad028106e0d856c2e8dd29563adb5
nullcss <nullyl@163.com>
2018-09-07 13:54:39 +0800

    css

--- components/content/content.js
+++ components/content/content.js
@@ -27,11 +27,12 @@ Component({
       wx.getLocation({
         type: 'gcj02', //返回可以用于wx.openLocation的经纬度
         success: function(res) {
+          console.log(res)
           var latitude = res.latitude
           var longitude = res.longitude
           wx.openLocation({
-            latitude: latitude,
-            longitude: longitude,
+            latitude: 34.75589,
+            longitude: 113.74808,
             scale: 28
           })
         }
