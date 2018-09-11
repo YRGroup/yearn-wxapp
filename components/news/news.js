30b72ecd22d512a221cbc9b87f6999d8ee05695e
xuchuan <825077792@qq.com>
2018-09-11 11:22:21 +0800

    news

--- components/news/news.js
+++ components/news/news.js
@@ -12,21 +12,13 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    count:{
-      type:Number,
-      value: 4
+    count: {
+      type: Number,
+      value: 3
     },
-    showTitle:{
-      type:Boolean,
-      value:false
-    },
-    showMore:{
-      type:Boolean,
-      value:false
-    },
-    showPagenation:{
-      type:Boolean,
-      value:true
+    listPage: {
+      type: Boolean,
+      value: true
     }
   },
 
@@ -34,7 +26,12 @@ Component({
    * 组件的初始数据
    */
   data: {
-    newsList: []
+    newsList: [],
+    currentPage: 1,
+    id: 2, //易尔动态的分类目录ID
+    loadingMore: false,
+    noMore: false
+
   },
   lifetimes: {
     // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
@@ -51,35 +48,64 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    getNews: function () {
+    //获取新闻
+    getNews: function (more) {
+
+      if (this.data.noMore) return;
+      //显示加载更多ing
+      if (more) {
+        this.setData({
+          loadingMore: true
+        })
+      }
       let para = {
         count: this.data.count,
-        id: 2
+        id: this.data.id,
+        currentPage: this.data.currentPage
       }
-      api.getNews(para, (res) => { 
-        let data=res.posts.map(el=>{
-          el.excerpt=this.formatNewsTitle(el.excerpt)
+      api.getNews(para, (res) => {
+        let data = res.posts.map(el => {
+          el.excerpt = this.formatNewsTitle(el.excerpt)
           return el;
         })
+        //显示没有更多text
+        if (!res.posts.length) {
+          this.setData({
+            noMore: true
+          })
+        }
+        //隐藏加载更多ing
+        if (more) {
+          this.setData({
+            loadingMore: false
+          })
+        }
         this.setData({
-          newsList: res.posts
+          newsList: this.data.newsList.concat(data)
         })
       });
     },
-    formatNewsTitle:(str)=>{
-      let reg=/^<.+>(.+)<.+>/;
-      return str.match(reg)[1]||'';
+    formatNewsTitle: (str) => {
+      let reg = /^<.+>(.+)<.+>/;
+      return str.match(reg)[1] || '';
     },
-    handlenews(ev){
-      let id= ev.currentTarget.dataset.id;
+    handlenews(ev) {
+      let id = ev.currentTarget.dataset.id;
       wx.navigateTo({
-        url:`../news/index?id=${id}`
+        url: `../news/index?id=${id}`
       })
     },
-    toNewsPage(){
+    toNewsPage() {
       wx.navigateTo({
-        url:`../newsList/index`
+        url: `../newsList/index`
       })
+    },
+    //下拉加载
+    handleScrollBottom() {
+      this.setData({
+        currentPage: ++this.data.currentPage
+      })
+      this.getNews(true)
     }
   }
 })
