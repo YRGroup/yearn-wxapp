77ac596c4a628f0b0eecc004a9f65ff8346c65b2
nullcss <nullyl@163.com>
2018-09-13 15:04:43 +0800

    css

---
+++ components/title/title.js
@@ -0,0 +1,30 @@
+// components/title/title.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text1: {
+      type: String,
+      value: 'Enlish',
+    },
+    text2: {
+      type: String,
+      value: 'Chinease',
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  api_blink_url: 'http://a1.yearn.com/',
+  api_blink_url: 'https://s1.yearn.com/',
   appkey: ''
 }
 
