12bfd5844ec58812bf7804b084fdeec3aa3596a3
xuchuan <825077792@qq.com>
2018-09-13 13:57:26 +0800

    images

--- components/design/design.js
+++ components/design/design.js
@@ -1,5 +1,6 @@
 // components/design/design.js
 let text = `圆融建筑结构特有的空间属性，在满足安全和功能的前提下，增强学生的体验和互动，启发学生主动思考、学习和探索。`
+const app = getApp();
 Component({
   /**
    * 组件的属性列表
@@ -12,8 +13,8 @@ Component({
    * 组件的初始数据
    */
   data: {
-    introduceBg: '../../images/index/designBg.jpg',
-    round: '../../images/index/round.png',
+    introduceBg: app.globalData.img_base_url + 'primary_bg_4.png',
+    round: app.globalData.img_base_url+'round.png',
     text: text
   },
 
