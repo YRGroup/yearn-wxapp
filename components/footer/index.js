e20ba68b957628baf064b0472ed93752da53d0c9
xuchuan <825077792@qq.com>
2018-09-25 16:02:37 +0800

    logo

---
+++ components/footer/index.js
@@ -0,0 +1,33 @@
+// components/content/content.js
+const app = getApp()
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    src:{
+      type:String,
+      value:''
+    }
+  },
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+    attached: function () {
+      
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    // codeImg: app.globalData.img_base_url + 'yearn_codeImg.jpg'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
